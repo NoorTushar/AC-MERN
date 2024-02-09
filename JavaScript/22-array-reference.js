e467ed0310b62637a60752399decf14d5efeeac9
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-09 16:33:00 +0600

    first commit

---
+++ JavaScript/22-array-reference.js
@@ -0,0 +1,7 @@
+let arr = ["a", "b"];
+let arrCopy = arr;
+
+arrCopy.push("c");
+arr.push("d");
+console.log(arr);
+console.log(arrCopy);
---
+++ JavaScript/23-Multiplication-Table.js
@@ -0,0 +1,18 @@
+// for loop, multiplication table of 10
+
+/**
+    for (let i = 1; i <= 10; i++) {
+        console.log(i * 10);
+    }
+ */
+
+// let our number be n
+// if n = 4
+
+/*
+    for (let i = 1; i <= 10; i++) {
+        console.log(i * 4);
+    }
+*/
+
+let n = prompt("Enter Your Number");
