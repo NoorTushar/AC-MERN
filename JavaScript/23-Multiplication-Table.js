a748d50e196943ed9c928b27b93b106e23911975
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-09 17:04:09 +0600

    multiplication table and user prompt

--- JavaScript/23-Multiplication-Table.js
+++ JavaScript/23-Multiplication-Table.js
@@ -15,4 +15,16 @@
     }
 */
 
-let n = prompt("Enter Your Number");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout,
+});
+
+rl.question("What is your the number you want? ", (n) => {
+   for (let i = 1; i <= 10; i++) {
+      console.log(i * n);
+   }
+   rl.close();
+});
